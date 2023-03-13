d5de5e054941c581148e81d3992f590a72588385
ahmed2nabil <ahmednabl992@gmail.com>
2023-03-13 14:40:41 +0200

    set up job controller

---
+++ controller/job.js
@@ -0,0 +1,19 @@
+exports.getALlJobs = async(req, res) => {
+    res.send("All Jobs");
+}
+
+exports.getJob = async(req, res) => {
+    res.send("get a Job");
+}
+
+exports.createJob = async(req, res) => {
+    res.send("create Job");
+}
+
+exports.updateJob = async(req, res) => {
+    res.send("udpate Job");
+}
+
+exports.deleteJob = async(req, res) => {
+    res.send("delete Job");
+}
