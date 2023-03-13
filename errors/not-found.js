91e291a18d179da82021d07a58d1baaaf012bd34
ahmed2nabil <ahmednabl992@gmail.com>
2023-03-13 13:39:15 +0200

    handle errors

---
+++ errors/not-found.js
@@ -0,0 +1,8 @@
+const {StatusCodes } = require("http-status-codes");
+const CustomAPIError = require("./custom-api");
+class NotFound extends CustomAPIError {
+    constructor(message){
+        super(message);
+        this.statusCode = StatusCodes.NOT_FOUND;
+    } 
+}
---
+++ errors/unauthenticated.js
@@ -0,0 +1,8 @@
+const {StatusCodes } = require("http-status-codes");
+const CustomAPIError = require("./custom-api");
+class UnauthenticatedError extends CustomAPIError {
+    constructor(message){
+        super(message);
+        this.statusCode = StatusCodes.UNAUTHORIZED;
+    } 
+}
