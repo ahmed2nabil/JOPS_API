d63b7e0df6c5acdd7e88525de22c63a5ab509734
ahmed2nabil <ahmednabl992@gmail.com>
2023-03-13 14:57:14 +0200

    set up jobs routes

---
+++ routes/jobs.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const router = express.Router();
+
+const jobsController= require("../controller/job");
+
+
+router.get("/",jobsController.getAllJobs);
+
+router.get("/:id",jobsController.getJob);
+
+router.post("/",jobsController.createJob);
+
+router.put("/:id",jobsController.updateJob);
+
+router.delete("/:id",jobsController.deleteJob);
+
+
+module.exports = router;
