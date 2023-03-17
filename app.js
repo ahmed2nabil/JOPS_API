9b5e24b4a6ec96768a30c318d66af335fd6f212f
ahmed2nabil <ahmednabl992@gmail.com>
2023-03-17 14:52:02 +0200

    add security

--- app.js
+++ app.js
@@ -4,6 +4,11 @@ const express = require('express');
 const app = express();
 
 
+// extra security packages
+const helmet = require('helmet');
+const cors = require('cors');
+const xss = require('xss-clean');
+const rateLimiter = require('express-rate-limit');
 
 //connectDb
 const connectDB = require("./db/connect");
@@ -16,8 +21,16 @@ const notFoundMiddleware  =  require("./middlewares/not-found");
 const errorHandlerMiddleware  =  require("./middlewares/error-handler");
 
 //packages
+app.set('trust proxy', 1);
+app.use(
+  rateLimiter({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 100, // limit each IP to 100 requests per windowMs
+  }));
 app.use(express.json());
-
+app.use(helmet());
+app.use(cors());
+app.use(xss());
 
 //routes 
 app.use("/api/v1/auth",authRouter);
